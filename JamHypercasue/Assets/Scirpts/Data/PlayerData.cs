2f8b15eb87c2ebdc8b5a3942c509cef7da004369
clément bostyn <clemclem20002@gmail.com>
2025-11-03 16:40:03 +0100

    [ADD] je sais pas frr

--- JamHypercasue/Assets/Scirpts/Data/PlayerData.cs
+++ JamHypercasue/Assets/Scirpts/Data/PlayerData.cs
@@ -11,6 +11,25 @@ public static class PlayerData
         }
         return PlayerPrefs.GetInt(ProjectConst.PlayerLevel);
     }
+    public static int GetSoftCurrency()
+    {
+        if (!PlayerPrefs.HasKey(ProjectConst.SoftCurrency))
+        {
+            PlayerPrefs.SetInt(ProjectConst.SoftCurrency, 0);
+        }
+        return PlayerPrefs.GetInt(ProjectConst.SoftCurrency);
+    }
+    public static int GetHardCurrency()
+    {
+        if (!PlayerPrefs.HasKey(ProjectConst.HardCurrency))
+        {
+            PlayerPrefs.SetInt(ProjectConst.HardCurrency, 0);
+        }
+        return PlayerPrefs.GetInt(ProjectConst.HardCurrency);
+    }
+    
+    public static void IncriseHardCurrency(int amount) => PlayerPrefs.SetInt(ProjectConst.HardCurrency, PlayerPrefs.GetInt(ProjectConst.HardCurrency) + amount);
+    public static void IncriseSoftCurrency(int amount) => PlayerPrefs.SetInt(ProjectConst.SoftCurrency, PlayerPrefs.GetInt(ProjectConst.SoftCurrency) + amount);
 
     public static void InccrementPlayerLevel() => PlayerPrefs.SetInt(ProjectConst.PlayerLevel, PlayerPrefs.GetInt(ProjectConst.PlayerLevel) + 1);
 }
