00b79d2e4ad8aa97998830452e8e3584f3031759
clément bostyn <clemclem20002@gmail.com>
2025-11-06 10:46:47 +0100

    [ADD] final push b4 getting reject

--- JamHypercasue/Assets/Scirpts/ButtonsShop.cs
+++ JamHypercasue/Assets/Scirpts/ButtonsShop.cs
@@ -19,13 +19,7 @@ public class ButtonsShop : MonoBehaviour
         return true;
     }
 
-    private void Awake()
-    {
-        foreach (Button button in buttons)
-        {
-            button.onClick.AddListener(ClickButton);
-        }
-    }
+    
 
     private void OnDestroy()
     {
@@ -35,9 +29,87 @@ public class ButtonsShop : MonoBehaviour
         }
     }
 
-    public void ClickButton()
+    public void ClickButtonHard(int price)
     {
-        if(CanBuySoft(10))
-            PlayerData.DeacriseSoftCurrency(10);
+        if (!CanBuyHard(price))
+            return;
+        
+        PlayerData.DeacriseHardCurrency(price);
+        if(price == 20)
+            PlayerData.IncriseSoftCurrency(250);
+        else if(price == 10)
+            PlayerData.IncriseSoftCurrency(100);
+        else if(price == 80)
+            PlayerData.IncriseSoftCurrency(1000);
     }
+    public void ClickButtonSoft(int price)
+    {
+        if (!CanBuySoft(price))
+            return;
+        
+        PlayerData.DeacriseSoftCurrency(price);
+        if (price == 5000)
+        {
+            if(PlayerPrefs.GetInt("Skin_3") == 1)
+                return;
+
+            //skin sayan rouge
+            PlayerPrefs.SetInt("Skin_3", 1);
+        }
+        else if (price == 10000)
+        {
+            if(PlayerPrefs.GetInt("Skin_2") == 1)
+                return;
+
+            PlayerPrefs.SetInt("Skin_2", 1);
+            //skin sayan gris
+        }
+    }
+
+    public void ClickButtonLuffy()
+    {
+        
+        if(PlayerPrefs.GetInt("Skin_5") == 1)
+            return;
+        
+        int price = 95;
+        if (!CanBuyHard(price))
+            return;
+        
+        PlayerPrefs.SetInt("Skin_5", 1);
+        PlayerData.DeacriseHardCurrency(price);
+    }
+    public void ClickButtonSayanGold()
+    {
+        if(PlayerPrefs.GetInt("Skin_4") == 1)
+            return;
+
+        int price = 75;
+        if (!CanBuyHard(price))
+            return;
+        
+        PlayerPrefs.SetInt("Skin_4", 1);
+        PlayerData.DeacriseHardCurrency(price);
+    }
+    public void ClickButtonNaruto()
+    {
+        if(PlayerPrefs.GetInt("Skin_1") == 1)
+            return;
+
+        int price = 200;
+        if (!CanBuyHard(price))
+            return;
+        
+        PlayerPrefs.SetInt("Skin_1", 1);
+        PlayerData.DeacriseHardCurrency(price);
+    }
+
+    public void ClickButtonAdd()
+    {
+        AdManager.Instance.ShowRewardedAd();
+        PlayerData.IncriseSoftCurrency(10);
+    }
+
+    
+    
 }
